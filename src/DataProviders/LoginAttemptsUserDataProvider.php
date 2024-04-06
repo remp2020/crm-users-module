2bd2cfdfea599af2145ff4c67c88e09f149b6fd5
Peter Dulacka <dulacka@gmail.com>
2024-04-06 21:55:45 +0200

    Add login attempts data to user data download
    remp/helpdesk#2621

--- src/DataProviders/LoginAttemptsUserDataProvider.php
+++ src/DataProviders/LoginAttemptsUserDataProvider.php
@@ -7,11 +7,9 @@ use Crm\UsersModule\Repositories\LoginAttemptsRepository;
 
 class LoginAttemptsUserDataProvider implements UserDataProviderInterface
 {
-    private $loginAttemptsRepository;
-
-    public function __construct(LoginAttemptsRepository $loginAttemptsRepository)
-    {
-        $this->loginAttemptsRepository = $loginAttemptsRepository;
+    public function __construct(
+        private readonly LoginAttemptsRepository $loginAttemptsRepository,
+    ) {
     }
 
     public static function identifier(): string
@@ -26,7 +24,31 @@ class LoginAttemptsUserDataProvider implements UserDataProviderInterface
 
     public function download($userId)
     {
-        return [];
+        $lastId = 0;
+        $query = $this->loginAttemptsRepository->getTable()
+            ->where('user_id = ?', $userId)
+            ->order('id ASC')
+            ->limit(1000);
+
+        $data = [];
+        while (true) {
+            $loginAttempts = (clone $query)->where('user_id > ?', $lastId)->fetchAll();
+            if (!count($loginAttempts)) {
+                break;
+            }
+
+            foreach ($loginAttempts as $loginAttempt) {
+                $lastId = $loginAttempt->id;
+                $data[] = [
+                    'created_at' => $loginAttempt->created_at->getTimestamp(),
+                    'status' => $loginAttempt->status,
+                    'source' => $loginAttempt->source,
+                    'ip' => $loginAttempt->ip,
+                ];
+            }
+        }
+
+        return $data;
     }
 
     public function downloadAttachments($userId)
